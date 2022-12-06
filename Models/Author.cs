0f8d0a63bc263bbee0f71a262d5c6d2fd57fc7b4
Anastasia Ropot <Admin@DESKTOP-QG8QPQ1>
2022-12-06 16:12:57 +0200

    ind

--- Models/Author.cs
+++ Models/Author.cs
@@ -4,13 +4,20 @@ namespace Ropot_Anastasia_Lab2.Models
 {
     public class Author
     {
-        public int ID { get; set;}
+        public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        [Display (Name ="Author")]
-        public string FullName { get; set; }
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + "  " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
-
     }
 }
+
+
